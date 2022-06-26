86c268eb67c7df574ea0b104792b26f80fcbb99b
sheilavazquezch <l17390367@chetumal.tecnm.mx>
2022-06-25 21:45:45 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectornumericoComponent } from './selectornumerico/selectornumerico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectornumericoComponent
   ],
   imports: [
     BrowserModule,
